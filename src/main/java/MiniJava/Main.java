2fb6a0be9e6d16931b08adbe3c2b187985424c90
ErfanMojibi <mojibierfan@gmail.com>
2025-05-23 02:00:49 -0700

    Facade: add ParserFacade

--- src/main/java/MiniJava/Main.java
+++ src/main/java/MiniJava/Main.java
@@ -5,14 +5,14 @@ import java.util.Scanner;
 
 import MiniJava.errorHandler.ErrorHandler;
 import MiniJava.parser.Parser;
+import MiniJava.parser.ParserFacade;
 
 
 public class Main {
     public static void main(String[] args) {
-        Parser parser = new Parser();
         try {
-            // start parsing
-            parser.startParse(new Scanner(new File("src/main/resources/code")));
+            ParserFacade parserFacade = new ParserFacade(new Scanner(new File("src/main/resources/code")));
+            parserFacade.startParsing();
         } catch (FileNotFoundException e) {
             ErrorHandler.printError(e.getMessage());
         }
